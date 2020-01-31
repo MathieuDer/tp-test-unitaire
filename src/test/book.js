f2641e42d2cf70e2804df9ff2a978e874626413d
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-01-31 12:33:26 +0100

    Tous les tests unitaires sont fonctionnels

--- src/test/book.js
+++ src/test/book.js
@@ -20,17 +20,40 @@ chai.use(chaiAsPromised);
 
 /*** TESTS UNITAIRES ***/
 
-describe('Tests unitaires', function () {
+describe('Tests unitaires', () => {
 
-    describe('Simulation de bonnes réponse', function () {
+    /**
+     * On devra s’assurer également que les intercepteurs soient clean entre chaque test.
+     */
+    afterEach(() => nock.cleanAll());
+
+    /**
+     * Premiere série de test (simulation de réponse ok)
+     */
+    describe('Simulation de bonnes réponse', () => {
 
         /** 
          * /GET
          * Que la réponse ait un status 200 
          * Que la clé books de la réponse simulé soit un array
          */
-        it('/GET Books array', function () {
-            //     expect('foo').to.be.a('string');
+        it('/GET Books array', done => {
+
+            nock("http://localhost:8080")
+                .get('/book')
+                .reply(200, { books: [] });
+
+            chai
+                .request('http://localhost:8080')
+                .get('/book')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.books).to.be.a('array');
+                    expect(res.body.books.length).to.equal(0);
+                    done();
+                });
+
         });
 
         /**
@@ -38,7 +61,29 @@ describe('Tests unitaires', function () {
          * Que la réponse ait un status 200
          * Que la clé message de la réponse simulé soit :  ‘book successfully added’
          */
-        it('/POST message "book successfully added"', function () {
+        it('/POST message "book successfully added"', done => {
+
+            nock("http://localhost:8080")
+                .post('/book')
+                .reply(200, { message: 'book successfully added' });
+
+            chai
+                .request('http://localhost:8080')
+                .post('/book')
+                .send({
+                    "id": "55b7d315-1a5f-4b13-a665-c382a6c71756",
+                    "title": "Oui-Oui contre Dominique Strauss-Kahn",
+                    "years": "2015",
+                    "pages": "650"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('book successfully added');
+                    done();
+                });
+
         });
 
         /**
@@ -46,7 +91,25 @@ describe('Tests unitaires', function () {
          * Que la réponse ait un status 200
          * Que la clé message de la réponse simulé soit :  ‘book successfully updated’
          */
-        it('/PUT message "book successfully updated"', function () {
+        it('/PUT message "book successfully updated"', done => {
+
+            nock("http://localhost:8080")
+                .put('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .reply(200, { message: 'book successfully updated' });
+
+            chai
+                .request('http://localhost:8080')
+                .put('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .send({
+                    "pages": "650"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('book successfully updated');
+                    done();
+                });
         });
 
         /**
@@ -54,56 +117,130 @@ describe('Tests unitaires', function () {
          * Que la réponse ait un status 200
          * Que la clé message de la réponse simulé soit :  ‘book successfully deleted’
          */
-        it('/DELETE message "book successfully deleted"', function () {
+        it('/DELETE message "book successfully deleted"', done => {
+
+            nock("http://localhost:8080")
+                .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .reply(200, { message: 'book successfully deleted' });
+
+            chai
+                .request('http://localhost:8080')
+                .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('book successfully deleted');
+                    done();
+                });
         });
 
     });
 
-    describe('Simulation de mauvaises réponses', function () {
+    /**
+     * Seconde série de test (simulation de mauvaise réponse)
+     */
+    describe('Simulation de mauvaises réponses', () => {
+
         /**
          * /GET
          * Que la réponse ait un status 400
          * Que la clé message de la réponse simulé soit :  ‘error fetching books’
          */
-        it('/GET message "error fetching books"', function () {
-        });
+        it('/GET message "error fetching books"', done => {
 
+            nock("http://localhost:8080")
+                .get('/book')
+                .reply(400, { message: 'error fetching books' });
+
+            chai
+                .request('http://localhost:8080')
+                .get('/book')
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('error fetching books');
+                    done();
+                });
+        });
 
         /**
          * /POST
          * Que la réponse ait un status 400
          * Que la clé message de la réponse simulé soit :  ‘error adding the book’
          */
-        it('/POST message "error adding the book"', function () {
-        });
+        it('/POST message "error adding the book"', done => {
 
+            nock("http://localhost:8080")
+                .post('/book')
+                .reply(400, { message: 'error adding the book' });
+
+            chai
+                .request('http://localhost:8080')
+                .post('/book')
+                .send({
+                    "id": "55b7d315-1a5f-4b13-a665-c382a6c71756",
+                    "title": "Oui-Oui contre Dominique Strauss-Kahn",
+                    "years": "2015",
+                    "pages": "650"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('error adding the book');
+                    done();
+                });
+        });
 
         /**
          * /PUT
          * Que la réponse ait un status 400
          * Que la clé message de la réponse simulé soit :  ‘error updating the book’
          */
-        it('/PUT message "error updating the book"', function () {
-        });
+        it('/PUT message "error updating the book"', done => {
 
+            nock("http://localhost:8080")
+                .put('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .reply(400, { message: 'error updating the book' });
+
+            chai
+                .request('http://localhost:8080')
+                .put('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .send({
+                    "pages": "650"
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('error updating the book');
+                    done();
+                });
+        });
 
         /**
          * /DELETE
          * Que la réponse ait un status 400
          * Que la clé message de la réponse simulé soit :  ‘error deleting the book’
          */
-        it('/DELETE message "error deleting the book"', function () {
+        it('/DELETE message "error deleting the book"', done => {
+
+            nock("http://localhost:8080")
+                .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .reply(400, { message: 'error deleting the book' });
+
+            chai
+                .request('http://localhost:8080')
+                .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+                .end((err, res) => {
+                    expect(res).to.have.status(400);
+                    expect(res.body).to.be.a('object');
+                    expect(res.body.message).to.be.a('string');
+                    expect(res.body.message).to.equal('error deleting the book');
+                    done();
+                });
         });
-
     });
-
-
 });
-
-
-
-
-
-
-// Seconde série de test (simulation de mauvaise réponse)
-
