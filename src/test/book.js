546a2f62f4a3247f38041fe08164dfae78bd7102
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2020-01-31 10:56:44 +0100

    Tests unitaires - Ajout de la structure

--- src/test/book.js
+++ src/test/book.js
@@ -15,3 +15,95 @@ chai.use(chaiAsPromised);
 // tout les packages et fonction nescessaire au test sont importé ici, bon courage
 
 // fait les Tests d'integration en premier
+
+
+
+/*** TESTS UNITAIRES ***/
+
+describe('Tests unitaires', function () {
+
+    describe('Simulation de bonnes réponse', function () {
+
+        /** 
+         * /GET
+         * Que la réponse ait un status 200 
+         * Que la clé books de la réponse simulé soit un array
+         */
+        it('/GET Books array', function () {
+            //     expect('foo').to.be.a('string');
+        });
+
+        /**
+         * /POST
+         * Que la réponse ait un status 200
+         * Que la clé message de la réponse simulé soit :  ‘book successfully added’
+         */
+        it('/POST message "book successfully added"', function () {
+        });
+
+        /**
+         * /PUT
+         * Que la réponse ait un status 200
+         * Que la clé message de la réponse simulé soit :  ‘book successfully updated’
+         */
+        it('/PUT message "book successfully updated"', function () {
+        });
+
+        /**
+         * /DELETE
+         * Que la réponse ait un status 200
+         * Que la clé message de la réponse simulé soit :  ‘book successfully deleted’
+         */
+        it('/DELETE message "book successfully deleted"', function () {
+        });
+
+    });
+
+    describe('Simulation de mauvaises réponses', function () {
+        /**
+         * /GET
+         * Que la réponse ait un status 400
+         * Que la clé message de la réponse simulé soit :  ‘error fetching books’
+         */
+        it('/GET message "error fetching books"', function () {
+        });
+
+
+        /**
+         * /POST
+         * Que la réponse ait un status 400
+         * Que la clé message de la réponse simulé soit :  ‘error adding the book’
+         */
+        it('/POST message "error adding the book"', function () {
+        });
+
+
+        /**
+         * /PUT
+         * Que la réponse ait un status 400
+         * Que la clé message de la réponse simulé soit :  ‘error updating the book’
+         */
+        it('/PUT message "error updating the book"', function () {
+        });
+
+
+        /**
+         * /DELETE
+         * Que la réponse ait un status 400
+         * Que la clé message de la réponse simulé soit :  ‘error deleting the book’
+         */
+        it('/DELETE message "error deleting the book"', function () {
+        });
+
+    });
+
+
+});
+
+
+
+
+
+
+// Seconde série de test (simulation de mauvaise réponse)
+
